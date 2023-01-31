9f0bc8bc4baeda078980cb7f3ac59fa1b3e52579
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-01-31 20:47:51 +0600

    jwt verification done

---
+++ utilities/verifyToken.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ success: false, message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      res.status(403).send({ success: false, message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
