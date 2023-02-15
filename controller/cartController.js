f40143bc0e3506ea91d2b9daa1fd9e2fd6f339fb
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-02-15 22:23:37 +0600

    item adding with new id in cart

--- controller/cartController.js
+++ controller/cartController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const cartDB = require("../model/cart_model");
 
 // post request functions
@@ -6,13 +7,21 @@ exports.addCartToDb = async (req, res, next) => {
   const email = req.params.email;
   const filter = { email: email, _id: id };
   const availableItem = await cartDB.findOne(filter);
-  console.log(availableItem);
-  if (availableItem) {
-    const update = { quantity: availableItem.quantity + 1 };
-    const options = { upsert: true };
 
+  if (availableItem) {
+    const options = { upsert: true };
+    const update = { quantity: availableItem.quantity + 1 };
     await cartDB.updateOne(filter, update, options);
+    return res.send({ success: false, message: "quantity increased" });
   }
+
+  delete req.body._id;
+  await cartDB.create({
+    _id: new mongoose.Types.ObjectId(),
+    ...req.body,
+    quantity: 1,
+  });
+  res.send({ success: true, message: "Item added to cart" });
 };
 
 // get request function
